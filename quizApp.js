ae658b230d5f6be8b435d057ef1cfe27e2fd6012
spencerwencer <spencerwise@spencers-mbp-3.home>
2014-09-23 19:56:04 -0600

    Finished QuizApp.js

--- quizApp.js
+++ quizApp.js
@@ -5,28 +5,41 @@
 
 //Create a User constructor that accepts name, email, password, and totalScore paramters and set them appropriatly
 
-  //code here
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+
+}
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty paramters
 
-  //code here
+var Question = function(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+
+}
 
 
 //Create a users Array which is going to hold all of our users.
 
-  //code here
-
+var users = [];
 
 //Let's say three people signed up for our service, create 3 intances of User and add each to the users Array
 
-  //code here
+var spencer = new User("Spencer", "spencerrwise@gmail.com", "bananana", "89");
+var rowan = new User("Rowan", "Rowandoutrejones@gmail.com", "notwt7", "76");
+var alex = new User("alex", "dalexbrown@gmail.com", "waddupfaygs", "31");
 
+users.push(spencer,rowan, alex);
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
-
+var questions = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -34,15 +47,23 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+var answersArray = [];
 
+var q1 = new Question("T/F: Inheritance is achieved in JavaScript through Prototypes?", answersArray , true, 7);
+var q2 = new Question("T/F: JavaScript is just a scripting version of Java", answersArray, false, 3);
+var q3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", answersArray, true, 5);
 
 //Now push all of your instances of Question into the questions Array
 
-  //code here
+questions.push(q1,q2,q3);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
+for(var i = 0; i < users.length; i++){
+	console.log(users[i]);
+}
+for(var i = 0; i < questions.length; i++){
+	console.log(questions[i]);
+}
 
